98e91946c613b25abb2c94078a02b66b6c0d9446
niharika-1999 <niharika.rao@ymedialabs.com>
2021-09-03 21:50:09 +0530

    Added:Extend to more than 3 parameters

--- src/com/bridgelabzGenerics/TestMaximum.java
+++ src/com/bridgelabzGenerics/TestMaximum.java
@@ -1,16 +1,23 @@
 package com.bridgelabzGenerics;
 
-//Given 3 Integers to find maximum
 public class TestMaximum <T extends Comparable<T>>
    {
 	
-	T a,b,c;//class variables
+	T a,b,c,d;//class variables
 	
 	public TestMaximum(T a,T b,T c)//Constructors for 3 variables
 	{
 		this.a=a;
 		this.b=b;
 		this.c=c;
+	}
+	
+	public TestMaximum(T a,T b,T c,T d)//Constructors for 4 variables
+	{
+		this.a=a;
+		this.b=b;
+		this.c=c;
+		this.d=d;
 	}
 	
 	
@@ -30,8 +37,34 @@ public class TestMaximum <T extends Comparable<T>>
 		return max;
 	}
 	
+	// Use of compareTo function for 3 variable
+	public static <T extends Comparable<T>> T max(T a,T b,T c,T d)
+	{
+		T max=a;
+		if(b.compareTo(max)>0)
+		{
+			max=b;
+		}
+		if(c.compareTo(max)>0)
+		{
+			max=c;
+		}
+		if(d.compareTo(max)>0)
+		{
+			max=d;
+		}
+		printMax(a,b,c,max);
+		return max;
+	}
+		
 	//Printing maximum value for 3 variable
 	public static<T> void printMax(T a,T b,T c,T max)
+	{
+		System.out.printf("Maximum Number is:",max);
+	}
+	
+	//Printing maximum value for 4 variable
+	public static<T> void printMax(T a,T b,T c,T d,T max)
 	{
 		System.out.printf("Maximum Number is:",max);
 	}
